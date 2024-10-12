3dbe73340e1fc64a4fe45025272979bea0a63b48
Angel <angelin09ozoz@gmail.com>
2024-10-12 12:03:52 -0600

    Implementación dellogin

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Navigate, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
+
+  const isAuthenticated = !!localStorage.getItem('token'); //verifica si el usuario es autenticado
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a>
-          Hola soy Angel
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <Routes>
+        {/* Ruta del login */}
+        <Route path="/login" element={isAuthenticated ? <Navigate to="/home" /> : <Login />} />
+        {/* Ruta del home (protegida) */}
+        <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
+        {/* Ruta del login */}
+        <Route path="*" element={<Navigate to={isAuthenticated ? "/home" : "/login"} />} />
+      </Routes>
+    </Router>
+
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,66 @@
+import { Box, Button, TextField } from '@mui/material'
 import React from 'react'
 
 function Login() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+
+    return (
+
+        <Box
+            sx={{
+                width: 500,
+                maxWidth: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minHeight: '80vh',
+                margin: 'auto',
+                padding: 2,
+                boxSizing: 'border-box'
+            }}
+        >
+
+            <h1>LOGIN</h1>
+
+            <Box
+                sx={{
+                    width: '100%',
+                    maxWidth: { xs: 300, sm: 400, md: 500 },
+                    padding: 2,
+                    boxSizing: 'border-box'
+                }}
+            >
+
+                <div>
+                    <TextField
+                        fullWidth
+                        label="Usuario"
+                        id="fullWidth"
+                        margin='normal'
+                    />
+                    <TextField
+                        fullWidth
+                        id="outlined-password-input"
+                        label="Contraseña"
+                        type="password"
+                        autoComplete="current-password"
+                        margin='normal'
+                    />
+                    <Button
+                        variant='contained'
+                        color='primary'
+                        fullWidth
+                        sx={{ mt: 2 }}
+                    >
+                        Iniciar Sesión
+                    </Button>
+                </div>
+
+            </Box>
+        </Box>
+
+    )
 }
 
 export default Login
